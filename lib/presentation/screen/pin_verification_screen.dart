28e69792acf4fee7adfeb18c65eec9be2fb5716f
saiful <saiful0687@gmail.com>
2024-03-19 01:06:36 -0400

    set password UI done

--- lib/presentation/screen/pin_verification_screen.dart
+++ lib/presentation/screen/pin_verification_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:pin_code_fields/pin_code_fields.dart';
+import 'package:task_manager/presentation/screen/set_password_screen.dart';
 import 'package:task_manager/presentation/screen/sign_in_screen.dart';
 import 'package:task_manager/presentation/widget/background_setup.dart';
 
@@ -15,49 +17,84 @@ class _PinVerificationScreenState extends State<PinVerificationScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: BackgroundSetup(
-        child: Column(
-          children: [
-            const SizedBox(
-              height: 120,
-            ),
-            Text(
-              "Pin Verification",
-              style: Theme.of(context).textTheme.titleLarge,
-            ),
-            const SizedBox(
-              height: 14,
-            ),
-            const Text(
-              "A 6 digit verification code will sent to your email address",
-              style: TextStyle(fontSize: 16, color: Colors.grey),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            TextFormField(
-                decoration: const InputDecoration(hintText: "OTP")),
-            const SizedBox(
-              height: 35,
-            ),
-            SizedBox(
-              width: double.infinity,
-              child: ElevatedButton(onPressed: (){}, child: Text("Verify")),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text("Have Account?"),
-                ElevatedButton(
-                    onPressed: () {
-                      Navigator.pushAndRemoveUntil(context,
-                          MaterialPageRoute(builder: (context){
-                            return SignInScreen();
-                          }), (route) => false);
-                    },
-                    child: const Text("Sign In"))
-              ],
-            )
-          ],
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 120,
+              ),
+              Text(
+                "Pin Verification",
+                style: Theme.of(context).textTheme.titleLarge,
+              ),
+              const SizedBox(
+                height: 14,
+              ),
+              const Text(
+                "A 6 digit verification code will sent to your email address",
+                style: TextStyle(fontSize: 16, color: Colors.grey),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              PinCodeTextField(
+                length: 6,
+                obscureText: false,
+                animationType: AnimationType.fade,
+                pinTheme: PinTheme(
+                  shape: PinCodeFieldShape.box,
+                  borderRadius: BorderRadius.circular(5),
+                  fieldHeight: 50,
+                  fieldWidth: 40,
+                  activeFillColor: Colors.white,
+                ),
+                animationDuration: Duration(milliseconds: 300),
+                backgroundColor: Colors.blue.shade50,
+                enableActiveFill: true,
+
+                onCompleted: (v) {
+                },
+                onChanged: (value) {
+                  setState(() {
+                  });
+                },
+                beforeTextPaste: (text) {
+                  print("Allowing to paste $text");
+                  //if you return true then it will show the paste confirmation dialog. Otherwise if false, then nothing will happen.
+                  //but you can show anything you want here, like your pop up saying wrong paste format or etc
+                  return true;
+                }, appContext: context,
+              ),
+              const SizedBox(
+                height: 35,
+              ),
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(onPressed: (){
+                  Navigator.push(context, MaterialPageRoute(
+                      builder: (context){
+                        return SetPasswordScreen();
+                      }));
+                }, child: Text("Verify")),
+              ),
+              SizedBox(height: 20,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text("Have Account?"),
+                  ElevatedButton(
+                      onPressed: () {
+                        Navigator.pushAndRemoveUntil(context,
+                            MaterialPageRoute(builder: (context){
+                              return SignInScreen();
+                            }), (route) => false);
+                      },
+                      child: const Text("Sign In"))
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
